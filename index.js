5d4cf33ba3c7ffdc4ba91218a9f7600d61860a5a
Miguel Angel <matr05viru@gmail.com>
2020-09-14 18:16:23 +0200

    feat: RomanNumber basic set up

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let { RomanNumber, dictionary } = require("./lib/index");
 console.log("Added entry point");
 console.log("dictionary", dictionary);
-console.log("RomanNumber", RomanNumber);
+console.log("RomanNumber creation with new", new RomanNumber("X"));
+console.log("RomanNumber creation without new", RomanNumber("X"));
