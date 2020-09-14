d96d556f8325b9b0183941aa6c077c580ecd4078
Miguel Angel <matr05viru@gmail.com>
2020-09-14 18:19:27 +0200

    feat: added toInt and toString functions

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ console.log("Added entry point");
 console.log("dictionary", dictionary);
 console.log("RomanNumber creation with new", new RomanNumber("X"));
 console.log("RomanNumber creation without new", RomanNumber("X"));
+
+console.log("RomanNumber toInt", RomanNumber("X").toInt());
+console.log("RomanNumber toString", RomanNumber("X").toString());
